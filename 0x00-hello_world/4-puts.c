1951b2fdcf892ea6b0d29fbcc5300b7977031e1b
myrajarenga <myrajarenga1234@gmail.com>
2022-09-09 03:33:31 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * main - print the sring in the put function
+ * main - print the string in the put function
  *
  * Description: using the main function
  * this program prints "Programming is like building a multilingual puzzle

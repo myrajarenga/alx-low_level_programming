2eac85d80ba160e835e14f060cef69d83ba05ece
myrajarenga <myrajarenga1234@gmail.com>
2022-09-09 02:42:24 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+*main - print the string in the put function
+*
+*Description using the main function
+*this program prints "Programming is like building amultilingualpuzzle"
+*Return:0
+*/
+int main (void)
+{
+        print("with proper grammar, but the outcome is apiece of art, \n");
+
+returns (0)
+

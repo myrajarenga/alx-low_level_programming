b84d48add9e18006af50b3d7ca0845606ab97113
myrajarenga <myrajarenga1234@gmail.com>
2022-09-09 05:40:11 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print the string in put function
+ *
+ * Description: using the main fuction
+ * this program prints :Programming is like building a mulyilingual puzzle
+ * Return: 0
+ */
+int main(void)
+{   
+	chat c;
+	int i;
+	log li;
+	long long lli
+	float f;
+
+printf("Size of a char: %Id bytes(s)\n", sizeof(c));
+printf("Size of an int: %Id bytes(s)\n", sizeof(i));
+printf("Size of a long int: %Id bytes(s)\n",sizeof(li));
+printf("Size of a long long int: %Id bytes(s)\n",sizeof(lli));
+printf("Size of a float: %Id byte(s)\n",sizeof(f));
+return (0);
+}

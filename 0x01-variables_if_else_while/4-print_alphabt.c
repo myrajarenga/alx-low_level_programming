3b4d6d36208dfe9022e1144cb7b0d5905feb40af
myrajarenga <myrajarenga1234@gmail.com>
2022-09-10 16:31:43 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main entry point
+ * Description print alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+int ch;
+for (ch = 'a' ; ch <= 'z'; ch++)
+{    	
+  if  (ch != 'e' && ch != 'q')
+}
+   {
+	   putchar(ch);
+    }
+ 
+putchar ('\n');
+ 
+  
+return (0);
+  
+}
+

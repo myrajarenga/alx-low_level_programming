4dd00436cb1348ce31ec9e90f34ae7d309cdadeb
myrajarenga <myrajarenga1234@gmail.com>
2022-09-11 10:53:23 -0700

    print alphabets

--- 0x01-variables_if_else_while/4-print_alphabet.c
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -7,18 +7,16 @@
  */
 int main(void)
 { 
-	int ch;
-	for (ch = 'a'; ch <= 'z'; ch++)
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
 
-        {
-		if (ch != 'e' && ch != 'q')
-		{
-		     putchar(ch);
-	        }
-                else
-		{
-		}
-        }
-        putchar('\n');
-	return (0);
+{
+if (ch != 'e' && ch != 'q')
+{
+	   putchar(ch);
+}
+               
+}
+putchar('\n');
+return (0);
 }	

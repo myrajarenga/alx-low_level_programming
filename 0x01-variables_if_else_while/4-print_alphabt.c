f07c501247b56e0733ec2eda93d4fa7c26ef0078
myrajarenga <myrajarenga1234@gmail.com>
2022-09-12 14:56:00 -0700

    print alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,18 +8,17 @@
 */
 int main(void)
 {
-		char a;
-		for (a = 'a' ; a <= 'z'; a++)
-		{    	
-			if  (a == 'q' || a == 'e')
+	char a;
 
-                 {
-			 continue;
-	         }
-			putchar(a);
-		
+	for (a = 'a' ; a <= 'z'; a++)
+	{
+		if  (a == 'q' || a == 'e')
+		{
+			continue;
 		}
-		putchar ('\n');
-		return (0);
+		putchar(a);
+	}
+	putchar ('\n');
+	return (0);
 }
 

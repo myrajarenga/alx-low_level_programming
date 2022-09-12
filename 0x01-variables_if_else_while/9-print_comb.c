a9217133bf421e09c48e294ea1ca9282eb3dfaa8
myrajarenga <myrajarenga1234@gmail.com>
2022-09-12 16:23:27 -0700

    -print_c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,25 +2,23 @@
 
 /**
  * main -entry point
- *
+ * Description: print number
  * Return: always return 0
- *
- * 
  */
 int main(void)
 {
-
 	int i;
-	for (i = 10; i < 20; i++)
-{
-	putchar(i);
 
-	if (i != 19)
+	for (i = 10; i < 20; i++)
 	{
-		putchar(',');
-		putchar(' ');
+		putchar(i);
+		if (i != 19)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
 	}
-}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }

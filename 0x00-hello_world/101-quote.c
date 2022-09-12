f26d4a049fffd105181c2f00f5f37bd532fe3cc5
myrajarenga <myrajarenga1234@gmail.com>
2022-09-12 02:21:33 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
 
+/**
+ *
+ * main -Entry point
+ * Description: prints Dor's quote
+ * Return: Always 1 (success)
+ */
+
+int main(void)
+{
+	write(2, "and that pice 0f art is useful\" - Dor Korpar, 2015-10-19\n");
+	return(1);
+}

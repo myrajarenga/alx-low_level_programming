40463dc3ee7ed0478e279ac3c314af4d9436a4d0
myrajarenga <myrajarenga1234@gmail.com>
2022-09-13 04:31:31 -0700

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,13 @@
 #include <unistd.h>
 
 /**
- *
- * main -print the string in the write function
- * Description: using the main function
- * this program prints " and that piece of art is useful\" - Dor's Korpar, 2015-10-19\"
+ * main - write the quote
+ * Description: print Dor's quote
  * Return: Always 1 (success)
  */
 
 int main(void)
 {
 	write(2, "and that pice 0f art is useful\" - Dor Korpar, 2015-10-19\n");
-	return(1);
+	return (1);
 }

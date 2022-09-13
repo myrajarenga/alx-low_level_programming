8459ee89624803499495ed6193dc5799915fcc41
myrajarenga <myrajarenga1234@gmail.com>
2022-09-13 04:37:27 -0700

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - write the quote
+ * main - entry point
  * Description: print Dor's quote
  * Return: Always 1 (success)
  */
 
 int main(void)
 {
-	write(2, "and that pice 0f art is useful\" - Dor Korpar, 2015-10-19\n");
+	write(2, "and that pice 0f art is useful\" - Dor Korpar, 2015-10-19\n", 59);
 	return (1);
 }

77c85259c3b853782df2429551796d5adad6c1b0
myrajarenga <myrajarenga1234@gmail.com>
2022-09-18 14:10:29 -0700

    my commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - print more numbers
+ *
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; j <= 9; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+			{
+				_putchar(i / 10 + '0');
+
+			}
+			_putchar(i % 10 + '0');
+
+		}
+		_putchar('\n');
+	}
+}

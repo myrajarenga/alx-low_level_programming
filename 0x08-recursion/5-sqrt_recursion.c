d2731725ed179f55412467c9386eb9eb568c4334
myrajarenga <myrajarenga1234@gmail.com>
2022-09-29 15:26:14 -0700

    implicit declaration of function ‘_sqr t

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - calculateb natural square root
+ * of anumber
+ * @n: number to calculate the square root
+ *
+ * return: the reul saquare root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (_sqrt(n, 0));
+}
+
+/**
+ * actual_sqrt_recursion - recurses to find the natural
+ * square root of a number
+ * @n: number to calculate the sqaure root of
+ * @i: iterator
+ *
+ * Return: the resulting square root
+ */
+int actual_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (_sqrt(n, i + 1));
+} 
+

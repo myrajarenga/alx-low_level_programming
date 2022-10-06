ca2d792930c0683b4f17bfe23a2aae4a54b5e150
myrajarenga <myrajarenga1234@gmail.com>
2022-10-06 14:10:12 -0700

    task 3

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,8 +7,8 @@ int _putchar(char);
 char *create_array(unsigned int size, char);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
-int **alloc_grind(int width, int height);
-void free_grind(int **grind, int height);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
 

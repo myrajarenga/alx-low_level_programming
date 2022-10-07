8ea4f1a45c308e8cec7afe15ac021be7c9fff27e
myrajarenga <myrajarenga1234@gmail.com>
2022-10-07 14:04:58 -0700

    'task o'

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* mallock_checked - allocate memory using malloc
+* @b:memory to allocate
+* Return: pointer to allocated memory or 
+* normal process termination if error
+*
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *s;
+
+	s = malloc(b);
+	if (s == NULL)
+		exit(98);
+
+	return (s);
+}

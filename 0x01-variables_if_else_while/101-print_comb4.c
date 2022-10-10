12be4272023ba0db13794577b675f3655cbf4334
myrajarenga <myrajarenga1234@gmail.com>
2022-10-10 04:53:06 -0700

    program that prints all possible different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,38 +8,36 @@
  */
 int main(void)
 {
-        int num1 = 48;
-        int num2;
-        int num3;
+	int num1 = 48;
+	int num2;
+	int num3;
+	int comma = 44;
 
-        int comma = 44; 
+	while (num1 <= 57)
+	{
+		num2 = num1 + 1;
 
-        while (num1 <= 57) 
-        {
-                num2 = num1 + 1;
+		while (num2 <= 57)
+		{
+			num3 = num2 + 1;
 
-                while (num2 <= 57) 
-                {
-                        num3 = num2 +1;
+			while  (num3 <= 57)
+			{
 
-                        while  (num3 <= 57)
-                        {
+				putchar(num1);
+				putchar(num2);
+				putchar(num3);
 
-                                putchar(num1);
-                                putchar(num2);
-                                putchar(num3);
-                                if (num1 !=55 || num2 !=56 || num3 !=57)
-                                {
-                                        putchar(comma);
-                                        putchar(32);
-                                }
-                                num3 += 1;
-                        }
-                        num2 += 1;
-                }
-
-                num1 += 1;
-        }
-
-        return (0);
-} 
+				if (num1 != 55 || num2 != 56 || num3 != 57)
+				{
+					putchar(comma);
+					putchar(32);
+				}
+				num3 += 1;
+			}
+			num2 += 1;
+		}
+		num1 += 1;
+	}
+	return (0);
+}

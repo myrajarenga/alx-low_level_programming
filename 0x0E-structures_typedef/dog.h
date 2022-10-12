ae0ff7e9df5e9ec55745803036c09fd83b9fb7bb
myrajarenga <myrajarenga1234@gmail.com>
2022-10-12 15:09:54 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * struct dog - struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */

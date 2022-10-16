94ddf1265d07b81f526344ea415103605e31ef1d
myrajarenga <myrajarenga1234@gmail.com>
2022-10-16 08:50:56 -0700

    task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,15 +2,15 @@
 #include <string.h>
 
 /**
- *get_op_func - selests the correct operation
+ *get_op_func - selects the correct operation
  *@s: operation to perform
- *Return: p;onter to the correct function
+ *Return: pointer to the correct function
  */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
 		{"+", op_add},
-		{"_", op_sub},
+		{"-", op_sub},
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},

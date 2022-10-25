80e0075dbcf97b4272fbe4394448a7aefdea1eec
myrajarenga <myrajarenga1234@gmail.com>
2022-10-25 07:35:53 -0700

    program to print list in link lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the start of the list
+ *
+ * Return: number of nodes in the list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t p = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+
+		printf("[0] (nil)");
+	else
+		printf("[%d] %s", h->len, h->str);
+		h = h->next;
+	p++;
+	}
+	return (p);
+
+}

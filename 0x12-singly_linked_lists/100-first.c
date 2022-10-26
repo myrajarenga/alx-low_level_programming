f92d7dd5f16fb6c7910c5406cc1b527e22cce792
myrajarenga <myrajarenga1234@gmail.com>
2022-10-26 09:55:53 -0700

    afunction that prints before the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * first - prints something before main
+ *
+ * Return: void
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
---
+++ 0x12-singly_linked_lists/100-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return:Always 0.
+ */
+int main(void)
+{
+	printf("(A tortoise, having pretty goodsense of a hare's narure,challenges oneto race.)\n");
+	return (0);
+}

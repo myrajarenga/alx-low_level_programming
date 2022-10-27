28bf9cd272e868e75c76cb237df3859203710a38
myrajarenga <myrajarenga1234@gmail.com>
2022-10-27 14:50:09 -0700

    function that adds anode at the end of alist

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+* add_nodeint_end - function that adds anew node at he end of alist
+* @head: head pointer
+* @n:number of node to be added
+*
+* Return:address of new elemnt or NULL if failed
+*/
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new;
+	listint_t *current = *head;
+
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+        	return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	current->next = new;
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
+	current->next = new;
+		return (new);
+}

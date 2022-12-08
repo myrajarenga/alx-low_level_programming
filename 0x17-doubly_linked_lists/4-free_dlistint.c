5ff12f6b90fc72301db90656988e91992c3e9f8b
myrajarenga <myrajarenga1234@gmail.com>
2022-12-08 12:48:52 -0800

    task4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_dlistint - Free the memory
+ * @head: Pointer to head
+ * Return: Nothing
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}

ad48a60497b259408e798c7e9d4bfd45b4c83b50
myrajarenga <myrajarenga1234@gmail.com>
2022-12-08 08:52:11 -0800

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,20 +6,6 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	/**size_t n = 0;
-	const dlistint_t *temp = h;
-
-	if (h == NULL)
-	{
-		return (0);
-	}
-	while (temp)
-	{
-		printf("%d\n", temp->n);
-		n++;
-		temp = temp->next;
-	}
-	return (n);*/
 
 	int counter = 0;
 
